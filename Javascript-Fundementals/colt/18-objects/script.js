77ebccfb6932f6cf929144d671fd35a26ce5f669
Leonard <spawnpoint13@gmail.com>
2024-01-30 22:28:16 +0000

    add study material

---
+++ Javascript-Fundementals/colt/18-objects/script.js
@@ -0,0 +1,60 @@
+
+// let fName = "leonard"
+
+// const fitBit = {
+//     totalFloors : 1826,
+//     totalMiles  : 211.70,
+//     name        : "John",
+//     firstName   : `${fName}`,
+//     Tags        : ["hello", "world"] //Add array in object
+// }
+
+// console.log(fitBit)
+// console.log(fitBit.totalFloors) //Access items in an array
+// console.log(fitBit["firstName"])
+
+
+/*---------------*/
+/*Modify Objects*/
+/*---------------*/
+
+// const fitBit = {
+//     totalFloors : 1826,
+//     totalMiles  : 211.70,
+//     name        : "John",
+//     firstName   : `dog`,
+//     Tags        : ["hello", "world"] //Add array in object
+// }
+
+
+// fitBit.totalFloors = 1200
+
+// console.log(fitBit)
+
+
+/*---------------*/
+/*Arrays and objects*/
+/*---------------*/
+
+
+const array1 =  [
+    { name: "Leonard",
+      age: 20,
+      color: "blue"
+    },
+    { name: "Leonard",
+      age: 20,
+      color: "blue"
+    },
+    { name: "Leonard",
+      age: 20,
+      color: "blue"
+    },
+    { name: "Leonard",
+      age: 20,
+      color: "blue"
+    }
+]
+
+console.log(array1[3].color)
+
---
+++ Javascript-Fundementals/colt/19-Repeating-things-with-loops/script.js
@@ -0,0 +1,78 @@
+/*-------------*/
+/* For loop*/
+/*-------------*/
+
+// for(i=1;i<=10;i++) {
+//     console.log(i)
+// }
+
+/*-------------*/
+/* For loop*/
+/* Print even Numbers*/
+/*-------------*/
+
+// for(i=1;i<=10;i=i+2) {
+//     console.log(i)
+// }
+
+
+/*-------------*/
+/* For loop*/
+/* Iterating through a array*/
+/*-------------*/
+
+// let array1 = ["dog", "cat", "rabbit"]
+
+// for(i=0;i<array1.length;i++){
+//     console.log(array1[i])
+// }
+
+
+/*-------------*/
+/* Nested Loops*/
+/*-------------*/
+
+// for (let i=1;i<=5;i++) {
+//     console.log(`the number is ${i}`);
+//     for (let j=1;j<=5;j++) {
+//         console.log(j);
+//     }
+// }
+
+// Example 2 - Iterate through an array
+
+// const array1 = [["dog", "cat", "rabbit"],
+//                ["mice", "rat", "ant"],
+//                ["god", "hocrux", "people"]
+
+// ]
+
+// for(let i=0;i<=array1.length-1;i++){
+//     let row = array1[i];
+//     for(let j=0;j<=row.length-1;j++){
+//         console.log(array1[i][j]);
+//     }
+// }
+
+/*-------------*/
+/* WHILE Loops*/
+/*-------------*/
+
+
+// let count = 0
+
+// while (count < 10) {
+//     console.log(count)
+//     count++;
+// }
+
+// Example 2s
+// const SECRETCODE = "LEONARD";
+
+// let message = prompt("Enter the password:");
+
+// while(message !== SECRETCODE){
+//     message = prompt("Your Password is Incorrect! Try again: ")
+// }
+
+// console.log("Congrats! you did it right!")
