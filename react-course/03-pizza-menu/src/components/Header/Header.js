f5cacb989138120f998e27770ff6624f996dc6ee
thedividedorange <spawnpoint13@gmail.com>
2024-09-11 16:25:29 +0100

    Fix: remove extra brackets

--- react-course/03-pizza-menu/src/components/Header/Header.js
+++ react-course/03-pizza-menu/src/components/Header/Header.js
@@ -2,10 +2,8 @@ import React from "react";
 
 export default function Header() {
   return (
-    <>
-      <header className="container-lg d-flex justify-content-center align-items-baseline mt-2 text-center fw-bold">
-        <h1 className="display-1 text-uppercase m-2">Pizza Menu</h1>
-      </header>
-    </>
+    <header className="container-lg d-flex justify-content-center align-items-baseline mt-2 text-center fw-bold">
+      <h1 className="display-1 text-uppercase m-2">Pizza Menu</h1>
+    </header>
   );
 }
