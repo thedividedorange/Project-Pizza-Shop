1bab9a8fe7e4906001f831909c659798c65044ab
thedividedorange <spawnpoint13@gmail.com>
2024-09-26 16:03:28 +0100

    compress assets

--- src/components/Header/HeaderOrderNow.js
+++ src/components/Header/HeaderOrderNow.js
@@ -4,7 +4,7 @@ import Layout from "./Layout";
 export default function HeadeOrderNow() {
   return (
     <Layout>
-      <h1 className="display-1 text-uppercase m-2">Order Below</h1>;
+      <h1 className="display-1 text-uppercase m-2">Order Below</h1>
     </Layout>
   );
 }
