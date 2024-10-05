7e938fb5cee0ffce774f7b3f3cb10a7592a93f12
Leonard Farmer <33452390+thedividedorange@users.noreply.github.com>
2024-10-05 23:31:17 +0100

    Update Promo.js
    Remove unused code

--- src/pages/home/Promo/Promo.js
+++ src/pages/home/Promo/Promo.js
@@ -1,31 +1,12 @@
 import React from "react";
-import Button from "components/Button/Button";
-import { useNavigate } from "react-router-dom";
 
 import "./Promo.css";
 
 export default function Promo() {
-  const navigate = useNavigate();
 
   return (
     <section id="promo" className="container-fluid">
-      <div className="container-xl">
-        <div className="row">
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-        </div>
-        <div className="row">
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-        </div>
-        <div className="row">
-          <div className="col"></div>
-          <div className="col"></div>
-          <div className="col"></div>
-        </div>
-      </div>
+    <div>todo</div>
     </section>
   );
 }
